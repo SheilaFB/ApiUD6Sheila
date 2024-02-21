6d1483505298204d8e176f313730af4a08e9a42f
sheila <sheila.feijoo@iessanmamede.com>
2024-02-21 15:22:47 +0100

    Inicio portafolio UD6

---
+++ src/main/java/com/example/apiud6sheila/ApiUd6SheilaApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiud6sheila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUd6SheilaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUd6SheilaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apiud6sheila/ApiUd6SheilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiud6sheila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUd6SheilaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
