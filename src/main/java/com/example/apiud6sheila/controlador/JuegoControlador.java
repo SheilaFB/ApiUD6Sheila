e00b80f496f6050e649d9e20c36d11c50e02fc34
sheila <sheila.feijoo@iessanmamede.com>
2024-02-22 10:56:55 +0100

    Controlador de juego con métodos para añadir, editar, listar, listar por orden alfabético y eliminar.

--- src/main/java/com/example/apiud6sheila/controlador/JuegoControlador.java
+++ src/main/java/com/example/apiud6sheila/controlador/JuegoControlador.java
@@ -3,6 +3,7 @@ package com.example.apiud6sheila.controlador;
 import com.example.apiud6sheila.modelo.Juego;
 import com.example.apiud6sheila.repositorio.JuegoRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +22,12 @@ public class JuegoControlador {
         return juegoRepositorio.findAll();
     }
 
+    //Listar todos los juegos por orden alfabético
+    @GetMapping("/alfabetico")
+    public List<Juego> listarAlfabetico(){
+        return juegoRepositorio.findAll(Sort.by(Sort.Direction.ASC,"nombre"));
+    }
+
     //Obtener un juego por su id
     @GetMapping("/{id}")
     public Juego obtenerJuego(@PathVariable long id){
@@ -32,17 +39,30 @@ public class JuegoControlador {
     //añadir un juego
     @PostMapping
     public Juego añadirJuego(@RequestBody Juego juego) {
-        return juegoRepositorio.save(juego);
+        if (juego.getNombre() instanceof String || !(juego.getNombre()==null) ||
+                juego.getPlataforma() instanceof String || !(juego.getPlataforma()==null)) {
+            return juegoRepositorio.save(juego);
+        }
+        return null;
     }
 
     //Editar un juego
     @PutMapping("/{id}")
     public Juego editarJuego(@PathVariable long id, @RequestBody Juego juego){
         return juegoRepositorio.findById(id).map(juegoTemp ->{
-            juegoTemp.setNombre(juego.getNombre());
-            juegoTemp.setPlataforma(juego.getPlataforma());
+            juegoTemp.setNombre((juego.getNombre()!=null)?juego.getNombre():juegoTemp.getNombre());
+            juegoTemp.setPlataforma((juego.getPlataforma()!=null)?juego.getPlataforma():juegoTemp.getPlataforma());
             return juegoRepositorio.save(juegoTemp);
-        }).orElseThrow();
 
+        }).orElseThrow(()->
+                new RuntimeException("Categoria no encontrada"));
     }
+
+    //Eliminar un juego
+    @DeleteMapping("/{id}")
+    public void eliminarJuego(@PathVariable long id){
+        juegoRepositorio.deleteById(id);
+    }
+
+
 }
--- src/main/java/com/example/apiud6sheila/modelo/Juego.java
+++ src/main/java/com/example/apiud6sheila/modelo/Juego.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity(name = "juegos")
 public class Juego {
@@ -11,8 +12,10 @@ public class Juego {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotBlank(message = "El nombre no puede estar vacío")
     private String nombre;
 
+    @NotBlank(message = "La plataforma no puede estar vacía")
     private String plataforma;
 
     public Juego(){}
