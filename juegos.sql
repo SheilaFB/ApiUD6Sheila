4a4b6d0ff37f9868496e46e59db205fea5867331
Sheila <marta@empresa.com>
2024-02-27 21:56:07 +0100

    Añadidos archivos sql para insertar los datos en las tablas. También se adjuntan los json.

---
+++ juegos.sql
@@ -0,0 +1,7 @@
+INSERT INTO juegos (nombre, plataforma, imagen) VALUES
+('Tetris', 'PC', 'https://play-lh.googleusercontent.com/ILMYT7PYDpF1771O6lixyX01a3JEGYKotMshaq6rFFxQnZPGb_byVbfgMZRJlG4pITJPfqoUC0g=w1296-h2160-rw'),
+('Pac-Man', 'PC', 'https://image.api.playstation.com/vulcan/ap/rnd/202109/2712/RqtJf2ccRcw4iBhjVXe2lsSq.jpg'),
+('Mario Kart', 'Nintendo Switch', 'https://i.blogs.es/acd781/mario-kart-8-deluxe/1366_2000.webp'),
+('Vampire Survivors', 'PC', 'https://img.asmedia.epimg.net/resizer/EUv4uOthEPCFm0qa47X2_ssG594=/1472x828/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/3BJUXU4SZRKJZFAKUZT73X4EVE.jpg'),
+('Temple Run', 'Android', 'https://i.ytimg.com/vi/H_2no6r5pag/maxresdefault.jpg'),
+('Subway Surfers', 'Android', 'https://cdn.mobilesyrup.com/wp-content/uploads/2019/12/subway-surfers.jpg');
